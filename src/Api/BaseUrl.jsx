05bedd5af2066202db1f7c6c858287e23d98ee7b
Mohammad Rehan Multani <136837218+ra-shi03@users.noreply.github.com>
2025-12-04 14:05:48 +0530

    api integrate

--- src/Api/BaseUrl.jsx
+++ src/Api/BaseUrl.jsx
@@ -1,5 +1,18 @@
 // const BaseUrl = 'https://84kmwvvs-4000.inc1.devtunnels.ms/api';
 
-const BaseUrl = 'https://ssknf82q-4000.inc1.devtunnels.ms/api';
+// const BaseUrl = 'https://ssknf82q-4000.inc1.devtunnels.ms/api';
+const BaseUrl = 'https://84kmwvvs-4000.inc1.devtunnels.ms/api';
+
 
 export default BaseUrl;
+
+// import axios from 'axios';
+
+// const BaseUrl = axios.create({
+//   baseURL: 'https://84kmwvvs-4000.inc1.devtunnels.ms/api',
+//   headers: {
+//     'Content-Type': 'application/json',
+//   }
+// });
+
+// export default BaseUrl;
